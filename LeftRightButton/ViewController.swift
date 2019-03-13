61fdc06c6a551aaf6ae37ee380d07c135798e4ba
dit03 <dit@imac-4.local>
2019-03-13 15:45:54 +0900

    Update

--- LeftRightButton/ViewController.swift
+++ LeftRightButton/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func Lbutton(_ sender: UIButton) {
+        myLabel.text = "Left Button Pressed"
+    }
+    
+    @IBAction func Rbutton(_ sender: UIButton) {
+        myLabel.text = "Right Button Pressed"
 
+    }
+    
 }
 
